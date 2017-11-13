ba03810d8a5d2b3f6924017d75de6c2f76584a84
Bruno Campos <bruno.campos@21mobile.com>
2017-11-13 09:43:41 -0200

    ajuste exemplo php

--- php/send.php
+++ php/send.php
@@ -2,6 +2,13 @@
 
 $curl = curl_init();
 
+$basic = base64_encode("USERNAME" . ":" . "PASSWORD");
+$messageText = "TESTE API v1 21MOBILE";
+$destination = "11999999999";
+$correlationId = "idDeControle";
+
+$json = json_encode(array("sms" => array(array("messageText" => $messageText, "destination" => $destination, "correlationId" => $correlationId))));
+
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.21mobile.com.br/v1/send",
   CURLOPT_RETURNTRANSFER => true,
@@ -10,12 +17,11 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\"sms\": [{\"messageText\" : \"Tesnte API v1 21mobile\", \"destination\": \"11999999999\", \"correlationId\" : \"2u76543\"}]}\n",
+  CURLOPT_POSTFIELDS => $json,
   CURLOPT_HTTPHEADER => array(
-    "authorization: Basic Zm9vOmJhcg==",
+    "authorization: Basic $basic",
     "cache-control: no-cache",
     "content-type: application/json",
-    "postman-token: 597bce62-a731-870e-b88d-b99ced1ba019"
   ),
 ));
 
